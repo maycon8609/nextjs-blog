b3db1405613b7319722f47b51ded1efa2c8f2dd2
maycon silva <maycon8609@gmail.com>
2024-11-13 16:22:15 -0300

    refactor: update post page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getPostData } from '../../../lib/posts';
+import { getPostData } from '@/lib/posts';
 import { notFound } from 'next/navigation';
 
 export default async function Post({ params }: { params: { id: string } }) {
@@ -9,10 +9,24 @@ export default async function Post({ params }: { params: { id: string } }) {
   }
 
   return (
-    <article>
-      <h1 className="text-2xl font-bold">{postData.title}</h1>
-      <p className="text-sm">{postData.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </article>
+    <main className="container mx-auto px-4 py-8 max-w-4xl">
+      <header className="mb-8">
+        <h1 className="text-4xl font-bold text-gray-100 mb-2">
+          {postData.title}
+        </h1>
+        <p className="text-gray-200 text-sm">
+          {new Date(postData.date).toLocaleDateString("pt-BR", {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })}
+        </p>
+      </header>
+
+      <section
+        className="max-w-none text-gray-200"
+        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+      />
+    </main>
   );
 }
