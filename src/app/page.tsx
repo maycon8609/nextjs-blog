0bc5a7d43fc58d56741bcb5e0c2b3c9ef28d4d8b
maycon silva <maycon8609@gmail.com>
2024-11-13 16:03:56 -0300

    refactor: update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card"; // Certifique-se de que estes componentes do shadcn/ui estão disponíveis
+import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card"; 
 
 interface RepoData {
   id: number;
@@ -8,6 +8,11 @@ interface RepoData {
   description: string;
   stargazers_count: number;
   language: string;
+  owner: {
+    login: string
+    avatar_url: string
+    html_url: string
+  }
 }
 
 async function fetchRepos(): Promise<RepoData[]> {
@@ -25,19 +30,37 @@ async function fetchRepos(): Promise<RepoData[]> {
   return response.json();
 }
 
+function getTopRepos(repos: RepoData[]): RepoData[] {
+  const reposWithDescription = repos.filter(repo => repo.description);
+  const reposWithoutDescription = repos.filter(repo => !repo.description);
+  
+  return [...reposWithDescription, ...reposWithoutDescription].slice(0, 12);
+}
+
 export default async function Home() {
   const repos = await fetchRepos();
-
-  console.log(repos.length)
+  const topRepos = getTopRepos(repos);
 
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-3xl font-bold my-6">Blog - v1</h1>
+    <div className="container mx-auto px-4 pb-6">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold my-6">
+          <Link href={topRepos[0].owner.html_url}>
+            {topRepos[0].owner.login}
+          </Link>
+        </h1>
+
+        <h1 className="text-3xl font-bold my-6">
+          <Link href="/posts">
+            Blog
+          </Link>
+        </h1>
+      </div>
       
-      <h2 className="text-2xl font-semibold mb-4">Meus Repositórios no GitHub</h2>
+      <h2 className="text-2xl font-semibold mb-4">Top GitHub Repositórios</h2>
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {repos.map((repo) => (
-          <Card key={repo.id} className="hover:shadow-lg transition-shadow duration-200">
+        {topRepos.map((repo) => (
+          <Card key={repo.id} className="hover:shadow-lg transition-shadow duration-200 flex flex-col justify-between min-h-44 max-h-80">
             <CardHeader>
               <CardTitle>
                 <Link href={repo.html_url} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-700 font-semibold">
